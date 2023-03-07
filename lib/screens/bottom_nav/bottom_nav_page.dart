b6dc7117c921ac5209a915b1b1a0441f5585f099
Asus <samandarahadjonovss@gmail.com>
2023-03-07 14:31:03 +0500

    btm nav bar done

--- lib/screens/bottom_nav/bottom_nav_page.dart
+++ lib/screens/bottom_nav/bottom_nav_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:ishonch/screens/bottom_nav/bloc/bottom_nav_cubit.dart';
+import 'package:ishonch/screens/bottom_nav/widgets/bottom_nav_bar.dart';
 import 'package:ishonch/utils/app_colors.dart';
 import 'package:ishonch/utils/app_image.dart';
 import 'package:ishonch/utils/text_style.dart';
@@ -15,80 +17,40 @@ class BottomNavPage extends StatefulWidget {
 }
 
 class _BottomNavPageState extends State<BottomNavPage> {
-  List<Widget> screens = [];
-
-  @override
-  void initState() {
-    screens.add(const Center(
+  List<Widget> screens = [
+    Center(
       child: Text(
         'Home',
         style: TextStyle(fontSize: 32),
       ),
-    ));
-    screens.add(const Center(
+    ),
+    Center(
       child: Text(
         'Favourite',
         style: TextStyle(fontSize: 32),
       ),
-    ));
-    screens.add(const Center(
-      child: Text(
-        'Profile',
-        style: TextStyle(fontSize: 32),
-      ),
-    ));
-    super.initState();
-  }
+    ),
+    Center(
+  child: Text(
+  'Profile',
+  style: TextStyle(fontSize: 32),
+  ),
+  )
+
+  ];
+
+
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => BottomNavCubit(),
-      child: BlocBuilder<BottomNavCubit, int>(
-        builder: (context, state) {
-          var index = context.watch<BottomNavCubit>().activePageIndex;
-          return Scaffold(
-            body: screens[index],
-            bottomNavigationBar: BottomNavigationBar(
-              selectedLabelStyle: fontRobotoW400(appcolor: AppColors.white)
-                  .copyWith(fontSize: 13.sp),
-              unselectedLabelStyle: fontRobotoW400(appcolor: AppColors.white)
-                  .copyWith(fontSize: 13.sp),
-              backgroundColor: AppColors.C_1F1914,
-              onTap: (value) => BlocProvider.of<BottomNavCubit>(context)
-                  .changePageIndex(value),
-              items: [
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    AppImages.iconHome,
-                    color: index == 0
-                        ? AppColors.white
-                        : AppColors.white.withOpacity(0.5),
-                  ),
-                  label: "",
-                ),
-                BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      AppImages.iconFavourite,
-                      color: index == 1
-                          ? AppColors.white
-                          : AppColors.white.withOpacity(0.5),
-                    ),
-                    label: ""),
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    AppImages.iconProfile,
-                    color: index == 2
-                        ? AppColors.white
-                        : AppColors.white.withOpacity(0.5),
-                  ),
-                  label: "",
-                ),
-              ],
-            ),
-          );
-        },
-      ),
+    return BlocBuilder<BottomNavCubit, int>(
+      builder: (context, state) {
+        var index = context.watch<BottomNavCubit>().activePageIndex;
+        return Scaffold(
+          body: screens[index],
+          bottomNavigationBar: BottomNavBar(),
+        );
+      },
     );
   }
 }
