d58e87350725470a9ad826aa78f82858f494d4d9
Asus <samandarahadjonovss@gmail.com>
2023-03-07 14:40:47 +0500

    get it done

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ishonch/app/app.dart';
 import 'package:ishonch/app/app_bloc_observer.dart';
 import 'package:ishonch/data/storage_repository/storage_repository.dart';
+import 'package:ishonch/service/get_it/get_it.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  setup();
   StorageRepository.getInstance();
   Bloc.observer = AppBlocObserver();
   runApp(App());
