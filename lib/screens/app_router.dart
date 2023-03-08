f5887438fcc9dde6c5d75cc06c183eaddb277b55
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-03-08 20:53:13 +0500

    some changes

--- lib/screens/app_router.dart
+++ lib/screens/app_router.dart
@@ -18,14 +18,6 @@ abstract class RouteName {
   static const languageChange = '/languageChange';
   static const about = '/about';
   static const checkOut = '/checkOut';
-  // static const register = 'register';
-  // static const forgot = 'forgot';
-  // static const home = 'home';
-  // static const productInfo = 'productInfo';
-  // static const search = 'search';
-  // static const checkout = 'checkout';
-  // static const successPayment = 'successPayment';
-  // static const profile = 'profile';
 }
 
 class AppRoutes {
@@ -35,46 +27,22 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => const SplashPage());
       case RouteName.onBoarding:
         return MaterialPageRoute(builder: (_) => const OnBoardingPage());
-      case RouteName.productByCategory:
-        return MaterialPageRoute(builder: (_) => const ProductByCategory());
       case RouteName.productDetail:
         return MaterialPageRoute(builder: (_) => const ProductDetailScreen());
       case RouteName.languageChange:
         return MaterialPageRoute(builder: (_) => const LanguageScreen());
       case RouteName.about:
         return MaterialPageRoute(builder: (_) => const AboutScreen());
+      case RouteName.productByCategory:
         return MaterialPageRoute(
           builder: (_) => ProductByCategory(
             data: settings.arguments as CategoryModel,
           ),
         );
-      case RouteName.productDetail:
-        return MaterialPageRoute(builder: (_) => ProductDetailScreen());
       case RouteName.checkOut:
         return MaterialPageRoute(builder: (_) => CheckOutScreen());
-      // case RoutName.register:
-      //   return MaterialPageRoute(builder: (_) => RegisterPage());
-      // case RoutName.forgot:
-      //   return MaterialPageRoute(builder: (_) => ForgotPage());
       case RouteName.bottomNavigation:
         return MaterialPageRoute(builder: (_) => const BottomNavPage());
-      // case RoutName.checkout:
-      //   return MaterialPageRoute(builder: (_) => CheckoutPaage());
-      // case RoutName.home:
-      //   return MaterialPageRoute(builder: (_) => HomePage());
-      // case RoutName.profile:
-      //   return MaterialPageRoute(builder: (_) => ProfilePage());
-      // case RoutName.successPayment:
-      //   return MaterialPageRoute(builder: (_) => SuccesPaymentPage());
-      // case RoutName.productInfo:
-      //   final args = settings.arguments as Map<String, dynamic>;
-      //   return MaterialPageRoute(
-      //     builder: (_) => ProductInfoPage(
-      //       productInfo: args['productInfo'],
-      //     ),
-      //   );
-      // case RoutName.search:
-      //   return MaterialPageRoute(builder: (_) => SearchPage());
       default:
         return MaterialPageRoute(builder: (_) => Scaffold());
     }
