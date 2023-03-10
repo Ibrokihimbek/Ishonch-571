09a9d76c8f40196d75a71ac6542a8c4fb3f7c8cd
Saidazam <saidolmovsaid@gmail.com>
2023-03-10 03:56:00 -0800

    changes

--- lib/screens/bottom_nav/bottom_nav_page.dart
+++ lib/screens/bottom_nav/bottom_nav_page.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
