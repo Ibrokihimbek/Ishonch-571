22f8a2f21eba824cdf0023501193f12dbcda0315
Saidazam <saidolmovsaid@gmail.com>
2023-03-10 05:09:56 -0800

    theme

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,5 @@
-import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -13,7 +13,6 @@ import 'package:ishonch/data/repositories/geocoding_repository.dart';
 import 'package:ishonch/screens/app_router.dart';
 import 'package:ishonch/screens/bottom_nav/bloc/bottom_nav_cubit.dart';
 import 'package:ishonch/service/api_service/api_service.dart';
-
 
 import '../utils/theme.dart';
 
@@ -43,12 +42,11 @@ class App extends StatelessWidget {
               OrdersBloc(CategoriesRepo(apiService: ApiService())),
         ),
         BlocProvider(
-          create: (context) =>
-              MapCubit(
-                geocodingRepo: GeocodingRepo(
-                  apiService: ApiService(),
-                ),
-              ),
+          create: (context) => MapCubit(
+            geocodingRepo: GeocodingRepo(
+              apiService: ApiService(),
+            ),
+          ),
         ),
         BlocProvider(
           create: (context) => DiscountCubit(),
@@ -72,21 +70,19 @@ class MyApp extends StatelessWidget {
             supportedLocales: context.supportedLocales,
             localizationsDelegates: context.localizationDelegates,
             locale: context.locale,
-            builder: (BuildContext context, Widget? child) =>
-                AdaptiveTheme(
-                  light: AppTheme.lightTheme,
-                  dark: AppTheme.darkTheme,
-                  initial: AdaptiveThemeMode.light,
-                  builder: (light, dark) =>
-                      MaterialApp(
-                        initialRoute: RouteName.splash,
-                        onGenerateRoute: AppRoutes.generateRoute,
-                        debugShowCheckedModeBanner: false,
-                        title: 'Ishonch 571',
-                        theme: light,
-                        darkTheme: dark,
-                      ),
-                ),
+            builder: (BuildContext context, Widget? child) => AdaptiveTheme(
+              light: AppTheme.lightTheme,
+              dark: AppTheme.darkTheme,
+              initial: AdaptiveThemeMode.light,
+              builder: (light, dark) => MaterialApp(
+                initialRoute: RouteName.splash,
+                onGenerateRoute: AppRoutes.generateRoute,
+                debugShowCheckedModeBanner: false,
+                title: 'Ishonch 571',
+                theme: light,
+                darkTheme: dark,
+              ),
+            ),
           );
         });
   }
