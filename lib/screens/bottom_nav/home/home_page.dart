357c33c372cd38cc595994bf8f564e0cbd17dc5f
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-03-11 19:49:03 +0500

    some changes

--- lib/screens/bottom_nav/home/home_page.dart
+++ lib/screens/bottom_nav/home/home_page.dart
@@ -1,18 +1,13 @@
-import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:ishonch/data/repositories/category_repo.dart';
 import 'package:ishonch/screens/bottom_nav/home/bloc/bloc_category/category_bloc.dart';
 import 'package:ishonch/screens/bottom_nav/home/bloc/bloc_category/category_event.dart';
 import 'package:ishonch/screens/bottom_nav/home/bloc/bloc_category/category_state.dart';
 import 'package:ishonch/screens/bottom_nav/home/widgets/widget/home_screen_appbar.dart';
 import 'package:ishonch/screens/bottom_nav/home/widgets/widget/home_widget.dart';
-import 'package:ishonch/screens/drawer/drawer.dart';
-import 'package:ishonch/service/api_service/api_service.dart';
-import 'package:ishonch/utils/app_image.dart';
-import 'package:ishonch/utils/my_utils.dart';
+
 
 class HomePage extends StatefulWidget {
   VoidCallback onTap;
