414d7f4148805773242f8440c4502b1e31ab33a5
Ibrokhimbek <ibrohim9902@gmail.com>
2023-03-17 18:14:38 +0500

    some changes

--- lib/screens/bottom_nav/home/widgets/categories_list.dart
+++ lib/screens/bottom_nav/home/widgets/categories_list.dart
@@ -20,61 +20,62 @@ class CategoriesList extends SliverPersistentHeaderDelegate {
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
     return Container(
-        padding: EdgeInsets.only(top: screenHeight * 0.01),
-        height: screenHeight * 0.15,
-        child: ListView.separated(
-          padding: const EdgeInsets.symmetric(horizontal: 12),
-          physics: const BouncingScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          itemCount: data.length,
-          itemBuilder: ((context, index) {
-            return GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, RouteName.productByCategory,
-                    arguments: data[index]);
-              },
-              child: Column(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: CachedNetworkImage(
-                      imageUrl:
-                          "http://146.190.207.16:3000/${data[index].categoryImage}",
-                      height: screenHeight * 0.09,
-                      width: screenWidth * 0.2,
-                      fit: BoxFit.cover,
-                      placeholder: (context, url) {
-                        return Shimmer.fromColors(
-                          period: const Duration(seconds: 2),
-                          baseColor: Colors.grey.shade300,
-                          highlightColor: Colors.grey.shade100,
-                          child: Container(
-                            width: 120,
-                            height: 100,
-                            color: Colors.white,
-                          ),
-                        );
-                      },
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error),
-                    ),
+      padding: EdgeInsets.only(top: screenHeight * 0.01),
+      height: screenHeight * 0.15,
+      child: ListView.separated(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        physics: const BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: data.length,
+        itemBuilder: ((buildContext, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, RouteName.productByCategory,
+                  arguments: data[index]);
+            },
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: CachedNetworkImage(
+                    imageUrl:
+                        "http://146.190.207.16:3000/${data[index].categoryImage}",
+                    height: screenHeight * 0.09,
+                    width: screenWidth * 0.2,
+                    fit: BoxFit.cover,
+                    placeholder: (context, url) {
+                      return Shimmer.fromColors(
+                        period: const Duration(seconds: 2),
+                        baseColor: Colors.grey.shade300,
+                        highlightColor: Colors.grey.shade100,
+                        child: Container(
+                          width: 120,
+                          height: 100,
+                          color: Colors.white,
+                        ),
+                      );
+                    },
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error),
                   ),
-                  SizedBox(height: screenHeight * 0.005),
-                  Text(
-                    data[index].categoryName,
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineMedium!
-                        .copyWith(fontSize: 14.sp),
-                  )
-                ],
-              ),
-            );
-          }),
-          separatorBuilder: (context, index) {
-            return SizedBox(width: screenWidth * 0.03);
-          },
-        ));
+                ),
+                SizedBox(height: screenHeight * 0.005),
+                Text(
+                  data[index].categoryName,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineMedium!
+                      .copyWith(fontSize: 14.sp),
+                )
+              ],
+            ),
+          );
+        }),
+        separatorBuilder: (context, index) {
+          return SizedBox(width: screenWidth * 0.03);
+        },
+      ),
+    );
   }
 
   @override
