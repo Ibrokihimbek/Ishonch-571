5e2341680c001b42997e16e457e21a4cf58bff18
Ibrokhimbek <ibrohim9902@gmail.com>
2023-03-19 23:31:13 +0500

    some changes

--- lib/screens/drawer/sub_screens/about_screen.dart
+++ lib/screens/drawer/sub_screens/about_screen.dart
@@ -1,13 +1,12 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:ishonch/screens/widgets/global_appbar.dart';
-import 'package:ishonch/utils/app_colors.dart';
+import 'package:ishonch/service/lat_long/lat_long.dart';
 import 'package:ishonch/utils/app_image.dart';
-import 'package:ishonch/utils/text_style.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
@@ -19,7 +18,7 @@ class AboutScreen extends StatelessWidget {
       body: Container(
         margin: const EdgeInsets.all(10).r,
         width: double.infinity,
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 30).r,
+        padding: const EdgeInsets.symmetric(vertical: 30).r,
         decoration: BoxDecoration(
             border: Border.all(color: Theme.of(context).shadowColor),
             borderRadius: BorderRadius.circular(15.r)),
@@ -44,13 +43,94 @@ class AboutScreen extends StatelessWidget {
               ),
             ),
             SizedBox(height: 10.h),
-            Text(
-              "about_text".tr(),
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge!
-                  .copyWith(fontSize: 16.sp, fontWeight: FontWeight.w500),
-            )
+            Padding(
+              padding: const EdgeInsets.all(16).r,
+              child: Text(
+                "about_text".tr(),
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(fontSize: 16.sp, fontWeight: FontWeight.w500),
+              ),
+            ),
+            const Spacer(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16).r,
+                  child: Text(
+                    'bizning_manzil'.tr(),
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontSize: 16.sp, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "Buyuk Ipak yo'li, Bekabad, Bekabad, Tashkent Region, Uzbekistan",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontSize: 16.sp, fontWeight: FontWeight.w500),
+                  ),
+                  trailing: ZoomTapAnimation(
+                    child: Icon(
+                      Icons.location_on,
+                      size: 32.sp,
+                      color: Theme.of(context).cardColor,
+                    ),
+                    onTap: () {
+                      LatLong.openMap(40.213734, 69.265442);
+                    },
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16.h),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16).r,
+                  child: Text(
+                    'telefon_raqam'.tr(),
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontSize: 16.sp, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "Abdulaziz\n+998 90 024 94 24",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontSize: 16.sp, fontWeight: FontWeight.w500),
+                  ),
+                  trailing: ZoomTapAnimation(
+                    child: Icon(
+                      Icons.phone,
+                      size: 32.sp,
+                      color: Theme.of(context).cardColor,
+                    ),
+                    onTap: () async {
+                      final Uri phoneUri =
+                          Uri(scheme: "tel", path: '+998900249424');
+                      try {
+                        if (await canLaunchUrl(phoneUri)) {
+                          await launchUrl(phoneUri);
+                        }
+                      } catch (error) {
+                        throw ("Cannot dial");
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
