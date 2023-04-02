3760fffc660f48f1c23ae9498f305dfdd0c13d21
rasulov999 <rasulovabdurashid89@gmail.com>
2023-04-02 23:31:42 +0500

    dfd

--- lib/screens/bottom_nav/home/widgets/home_widget.dart
+++ lib/screens/bottom_nav/home/widgets/home_widget.dart
@@ -12,7 +12,7 @@ import 'discount_widget.dart';
 class HomeWidget extends StatelessWidget {
   final List<CategoryModel> data;
 
-   HomeWidget({super.key, required this.data});
+   const HomeWidget({super.key, required this.data});
 
   @override
   Widget build(BuildContext context) {
